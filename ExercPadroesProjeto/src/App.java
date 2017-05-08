4a99c1b0ddb105ad545afeeea92a3a215e447d90
ViniKroth <vinicius.kroth@acad.pucrs.br>
2017-05-08 19:07:29 -0300

    Feitoria show de bola

--- ExercPadroesProjeto/src/App.java
+++ ExercPadroesProjeto/src/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -6,62 +7,57 @@ public class App {
 		Scanner in = new Scanner(System.in);
 
 		PassaporteFactory passaporteFactory = new PassaporteFactory();
-		String tipoDePassaporte = "";
-		Passaporte passaporte = null;
-		
+
+		Passaporte passaporte;
+
 		String nomeCliente = "";
 		int nroDias;
 		double valorBasico = 100;
 		int dia;
 		int mes;
 		int ano;
+		ArrayList<String> lista = new ArrayList<>();
 
-		//########################################################################
-		
+		// ########################################################################
+
 		System.out.println("######### FORMULÁRIO COMPRA PASSAPORTE #########\n");
-		
+
 		System.out.println("\nNome do cliente:");
 		nomeCliente = in.nextLine();
-		
+
 		System.out.println("\nNumero de dias:");
 		nroDias = Integer.parseInt(in.nextLine());
-		
+
 		System.out.println("\n--Data de Entrada--");
-		
+
 		System.out.println("\nDia:");
 		dia = Integer.parseInt(in.nextLine());
-		
+
 		System.out.println("\nMes:");
 		mes = Integer.parseInt(in.nextLine());
-		
+
 		System.out.println("\nAno:");
 		ano = Integer.parseInt(in.nextLine());
-		
-		//#########################################################################
-		
-		System.out.println("Por favor, informe E, S, P, ou I");
-		while(in.hasNextLine()){
-			tipoDePassaporte = in.nextLine();
-			
-			passaporte = passaporteFactory.fazPassaporte(tipoDePassaporte, nomeCliente, nroDias, valorBasico, dia, mes, ano);
-			
-			if(passaporte == null){					
-				System.out.println("Por favor, informe E, S, P, ou I");
-			}
-			
-		}
-		
-		System.out.println("Valor Total: " + passaporte.valorTotal());
-		
 
-		Passaporte passaporteBasico = new EstudanteUniversitario(new PassaporteBasico
-				(nomeCliente, nroDias, valorBasico, dia, mes, ano));
-		
-		
-	}
-	
-	public static void recursao(){
-		
+		// #########################################################################
+
+		System.out.println("Por favor, informe  E, S, P, ou I, para sair digite exit ");
+		String entra="";
+		int cont = 0;
+		while (!entra.equals("exit") || cont<4) {
+			entra = in.nextLine();
+			if (entra.equalsIgnoreCase("E") || entra.equalsIgnoreCase("S") || entra.equalsIgnoreCase("P")
+					|| entra.equalsIgnoreCase("I"))  lista.add(entra.toUpperCase());
+			cont++;	
+                 }
+
+		passaporte = passaporteFactory.fazPassaporte(nomeCliente, nroDias, valorBasico, dia, mes, ano, lista);
+
+		System.out.println("Valor Total: " + passaporte.valorTotal());
+
+		Passaporte passaporteBasico = new EstudanteUniversitario(
+				new PassaporteBasico(nomeCliente, nroDias, valorBasico, dia, mes, ano));
+
 	}
 
 }
--- ExercPadroesProjeto/src/PassaporteFactory.java
+++ ExercPadroesProjeto/src/PassaporteFactory.java
@@ -1,23 +1,35 @@
+import java.util.ArrayList;
 
 public class PassaporteFactory {
 
-	public Passaporte fazPassaporte(String novoTipoPassaporte, String nomeCliente, int nroDias, double valorBasico,
-			int dia, int mes, int ano) {
+	public Passaporte fazPassaporte(String nomeCliente, int nroDias, double valorBasico, int dia, int mes, int ano,
+			ArrayList<String> lista) {
 
-		switch (novoTipoPassaporte.toUpperCase()) {
-		case "E":
-			return new EstudanteUniversitario(new PassaporteBasico(nomeCliente, nroDias, valorBasico, dia, mes, ano));
-		case "S":
-			return new SeguroGarantido(new PassaporteBasico(nomeCliente, nroDias, valorBasico, dia, mes, ano));
-		case "P":
-			return new CCPagueBem(new PassaporteBasico(nomeCliente, nroDias, valorBasico, dia, mes, ano));
-		case "I":
-			return new CCPagueBem(new PassaporteBasico(nomeCliente, nroDias, valorBasico, dia, mes, ano));
-		case "ES" : 
-		default:
-			break;
+		PassaporteBasico passaporte = new PassaporteBasico(nomeCliente, nroDias, valorBasico, dia, mes, ano);
 
+		for (String s : lista) {
+			if(s.equals("E")) 
+			
 		}
+
+		// switch (novoTipoPassaporte.toUpperCase()) {
+		// case "E":
+		// return new EstudanteUniversitario(new PassaporteBasico(nomeCliente,
+		// nroDias, valorBasico, dia, mes, ano));
+		// case "S":
+		// return new SeguroGarantido(new PassaporteBasico(nomeCliente, nroDias,
+		// valorBasico, dia, mes, ano));
+		// case "P":
+		// return new CCPagueBem(new PassaporteBasico(nomeCliente, nroDias,
+		// valorBasico, dia, mes, ano));
+		// case "I":
+		// return new CCPagueBem(new PassaporteBasico(nomeCliente, nroDias,
+		// valorBasico, dia, mes, ano));
+		// case "ES" :
+		// default:
+		// break;
+		//
+		// }
 		return null;
 	}
 
